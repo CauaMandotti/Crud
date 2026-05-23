54a514b796387040b6b3f57d25f4b464bc2ac292
Caua Picoli Mandotti <ra2457072>
2026-05-23 08:58:19 -0300

    feat: implementa CRUD de Alunos (Aula 3 - Crediaria)

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,10 @@ Route::post('/admin/cursos/salvar', ['as'=>'admin.cursos.salvar','uses'=>'App\Ht
 Route::get('/admin/cursos/editar/{id}', ['as'=>'admin.cursos.editar','uses'=>'App\Http\Controllers\Admin\CursoController@editar']);
 Route::put('/admin/cursos/atualizar/{id}', ['as'=>'admin.cursos.atualizar','uses'=>'App\Http\Controllers\Admin\CursoController@atualizar']);
 Route::get('/admin/cursos/excluir/{id}', ['as'=>'admin.cursos.excluir','uses'=>'App\Http\Controllers\Admin\CursoController@excluir']);
+
+Route::get('/admin/alunos', ['as'=>'admin.alunos','uses'=>'App\Http\Controllers\Admin\AlunoController@index']);
+Route::get('/admin/alunos/adicionar', ['as'=>'admin.alunos.adicionar','uses'=>'App\Http\Controllers\Admin\AlunoController@adicionar']);
+Route::post('/admin/alunos/salvar', ['as'=>'admin.alunos.salvar','uses'=>'App\Http\Controllers\Admin\AlunoController@salvar']);
+Route::get('/admin/alunos/editar/{id}', ['as'=>'admin.alunos.editar','uses'=>'App\Http\Controllers\Admin\AlunoController@editar']);
+Route::put('/admin/alunos/atualizar/{id}', ['as'=>'admin.alunos.atualizar','uses'=>'App\Http\Controllers\Admin\AlunoController@atualizar']);
+Route::get('/admin/alunos/excluir/{id}', ['as'=>'admin.alunos.excluir','uses'=>'App\Http\Controllers\Admin\AlunoController@excluir']);
